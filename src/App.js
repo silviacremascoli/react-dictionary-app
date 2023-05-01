0312d38439d325fd29d6de064ce189f19b3e9de7
silviacremascoli <silcremascoli@gmail.com>
2023-05-01 10:48:15 +0200

    Added margin and h1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <h1>Dictionary App</h1>
     </div>
   );
 }

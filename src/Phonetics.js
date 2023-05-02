525b99102fe4fce2a351279535e0d52a2e3c3706
silviacremascoli <silcremascoli@gmail.com>
2023-05-02 17:29:24 +0200

    Added feature to phonetics component

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,14 +2,25 @@ import React from "react";
 import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetics(props) {
-  return (
-    <div className="Phonetics">
-      <div className="PhoneticsText">
-        <h4>{props.phonetics.text}</h4>
+  console.log(props.phonetics);
+  if (props.phonetics.audio) {
+    return (
+      <div className="Phonetics">
+        <div className="PhoneticsText">
+          <h4>{props.phonetics.text}</h4>
+        </div>
+        <div className="PhoneticsAudio">
+          <ReactAudioPlayer src={props.phonetics.audio} controls />
+        </div>
       </div>
-      <div className="PhoneticsAudio">
-        <ReactAudioPlayer src={props.phonetics.audio} controls />
+    );
+  } else {
+    return (
+      <div className="Phonetics">
+        <div className="PhoneticsText">
+          <h4>{props.phonetics.text}</h4>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }

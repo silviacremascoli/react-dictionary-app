4f64d92a1328fb3343e1cbbcf2073ce05f0ac939
silviacremascoli <silcremascoli@gmail.com>
2023-05-03 16:33:09 +0200

    Fixed synonyms component

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,19 +2,19 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  if (props.synonyms) {
+  if (props.synonyms.length) {
     return (
       <div className="Synonyms">
-        {props.synonyms.map(function (synonym, index) {
-          return (
-            <div className="d-flex inline-block justify-content-center">
-              <span className="see-also">See also: </span>
-              <ul key="index">
-                <li className="align-top">{synonym}</li>
-              </ul>
-            </div>
-          );
-        })}
+        <span className="see-also">See also: </span>
+        <ul>
+          {props.synonyms.map(function (synonym, index) {
+            return (
+              <li key={index} className="align-top">
+                {synonym}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   } else {

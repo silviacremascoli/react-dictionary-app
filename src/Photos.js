0f350b1d64254210488542564e7982f383bdef51
silviacremascoli <silcremascoli@gmail.com>
2023-05-04 11:07:22 +0200

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -2,18 +2,17 @@ import React from "react";
 import "./Photos.css";
 
 export default function Photos(props) {
-  if (props.photos.length) {
+  if (props.photos) {
     return (
       <section className="Photos">
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-md-4">
+              <div className="col-md-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.landscape}
                     alt={photo.alt}
-                    key={index}
                     className="img-fluid"
                   />
                 </a>
